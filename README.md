5f52118f5b2f5c6c92be164f9aff5626071a6f62
Frank Escobar <fescobar.systems@gmail.com>
2020-12-06 17:07:10 +0000

    SpecFlow3Net48 - Mono

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SpecFlow3Net48
+# SpecFlow3Net48 - Mono
+Project example to execute SpecRun.exe using mono
+
+
+Go to the root directory project and execute this:
+```sh
+docker-compose up
+```
---
+++ SpecFlow3Net48/StepDefinitions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow3Net48
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public StepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+        }
+    }
+}
